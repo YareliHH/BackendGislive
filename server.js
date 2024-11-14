920baea92ecdbf09c55b373cab52b28e616ac2b8
YareliHH <yhh122004@gmail.com>
2024-11-14 13:15:17 -0600

    si

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); 
-const Registrer = require('./routes/CRUDregistre.js'); 
-const login = require('./routes/CRUDuser.js');  
-const TerminosYC = require('./routes/CrudTerminosYC.js'); 
+const cors = require('cors');
+const Registrer = require('./routes/CRUDregistre.js');
+const login = require('./routes/CRUDuser.js');
+const TerminosYC = require('./routes/CrudTerminosYC.js');
 const politicas = require('./routes/CrudPoliticas.js');
 const deslinde = require('./routes/CrudDeslinde.js');
-const perfil_empresa=require('./routes/PerfiEmpresa.js');
-const redesSociales =require('./routes/RedesSociales.js');
-
-
+const perfil_empresa = require('./routes/PerfiEmpresa.js');
+const redesSociales = require('./routes/RedesSociales.js');
 
 const app = express();
-const router = express.Router(); // Asegúrate de definir el router
 
-app.use(cors());
+// Configuración de CORS para permitir solicitudes desde dominios específicos
+app.use(cors({
+    origin: [
+        'https://gisliveboutique.onrender.com',
+        'https://gisliveboutique.isoftuthh.com',
+        'https://backendgislive.onrender.com'
+    ], // Dominios permitidos
+    credentials: true  // Permitir el envío de cookies y credenciales
+}));
+
+// Middleware para parsear el cuerpo de las solicitudes en formato JSON
 app.use(bodyParser.json());
 
-app.use('/api', Registrer); 
+// Definir las rutas de la API
+app.use('/api', Registrer);
 app.use('/api', login);
 app.use('/api', TerminosYC);
-app.use('/api', TerminosYC);
 app.use('/api', politicas);
-app.use('/api', deslinde); 
-app.use('/api/',perfil_empresa);
-app.use('/api/',redesSociales);
+app.use('/api', deslinde);
+app.use('/api', perfil_empresa);
+app.use('/api', redesSociales);
 
-
-
-
+// Iniciar el servidor en el puerto especificado o en el puerto 3001 por defecto
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
-
-
-module.exports = router;
-
-
