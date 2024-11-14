13415ddcce75c897d148b6f33dc165f2a4279d9e
YareliHH <yhh122004@gmail.com>
2024-11-14 13:47:35 -0600

    cors

--- server.js
+++ server.js
@@ -6,35 +6,35 @@ const login = require('./routes/CRUDuser.js');
 const TerminosYC = require('./routes/CrudTerminosYC.js'); 
 const politicas = require('./routes/CrudPoliticas.js');
 const deslinde = require('./routes/CrudDeslinde.js');
-const perfil_empresa=require('./routes/PerfiEmpresa.js');
-const redesSociales =require('./routes/RedesSociales.js');
-
-
+const perfil_empresa = require('./routes/PerfiEmpresa.js');
+const redesSociales = require('./routes/RedesSociales.js');
 
 const app = express();
-const router = express.Router(); // Asegúrate de definir el router
 
-app.use(cors());
+// Configuración de CORS para permitir solicitudes desde dominios específicos
+app.use(cors({
+    origin: [
+        'https://gisliveboutique.onrender.com',
+        'https://gisliveboutique.isoftuthh.com',
+        'https://backendgislive.onrender.com'
+    ], 
+    credentials: true  // Permitir el envío de cookies y credenciales
+}));
+
+// Middleware para analizar el cuerpo de las solicitudes en formato JSON
 app.use(bodyParser.json());
 
+// Definir las rutas de la API
 app.use('/api', Registrer); 
 app.use('/api', login);
 app.use('/api', TerminosYC);
-app.use('/api', TerminosYC);
 app.use('/api', politicas);
 app.use('/api', deslinde); 
-app.use('/api/',perfil_empresa);
-app.use('/api/',redesSociales);
+app.use('/api', perfil_empresa);
+app.use('/api', redesSociales);
 
-
-
-
+// Iniciar el servidor en el puerto especificado o en el puerto 3001 por defecto
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
-
-
-module.exports = router;
-
-
